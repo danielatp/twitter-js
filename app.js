7a6c6b63643cd2c45f5db0b17ccc6f5479de992f
Daniela Tizon <daniela@MacBook-Pro-de-Daniela.local>
2017-11-07 11:57:37 -0500

    local page rendering cool

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require( 'express' );
 const app = express();
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 
 const PORT = 3000;
@@ -7,17 +9,35 @@ app.listen(PORT, () => {
   console.log('server listening');
 })
 
-app.use(function (req, res, next) {
-  // do your logging here
-  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-  console.log('Request Type:', req.method, req.method.res);
-  next()
-})
+app.use(volleyball)
 
 app.get('/', (req, res) => {
-  res.send('Welcomeeee');
+  // res.render('index', function(err, html){
+  //   res.send(html);
+  // });
+  res.render('index.html', locals, function (err, output) {
+    res.send(output);
+  });
 });
 
 app.get('/news', (req, res) => {
   res.send('news');
 })
+
+
+var locals = {
+  title: 'An Example',
+  people: [
+      { name: 'Gandalf'},
+      { name: 'Frodo' },
+      { name: 'Hermione'}
+  ]
+};
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+  console.log(output);
+});
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
