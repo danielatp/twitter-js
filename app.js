011de10f578ad6d8bce4b3e41e1bd15a897b4abc
Daniela Tizon <daniela@MacBook-Pro-de-Daniela.local>
2017-11-07 10:25:55 -0500

    twitter initialize

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ app.listen(PORT, () => {
   console.log('server listening');
 })
 
+app.use(function (req, res, next) {
+  // do your logging here
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  console.log('Request Type:', req.method, req.method.res);
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  res.send('Welcomeeee');
 });
 
-
+app.get('/news', (req, res) => {
+  res.send('news');
+})
